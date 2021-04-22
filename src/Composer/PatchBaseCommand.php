2f41a488333d1a9f9e7985e93fb75398465b240a
Stephan Zeidler <stephan@ramsalt.com>
2021-04-22 20:09:09 +0200

    Remove custom changing working directory
    Composer already handles this for us.
    
    Fixes #19

--- src/Composer/PatchBaseCommand.php
+++ src/Composer/PatchBaseCommand.php
@@ -11,24 +11,7 @@ class PatchBaseCommand extends BaseCommand {
   const PATCHTYPE_FILE = 2;
 
   protected function configure() {
-    $this->changeWorkingDirToComposerRoot();
-  }
-
-  protected function changeWorkingDirToComposerRoot() {
-    $io = $this->getIO();
-    $dir = dirname(getcwd());
-    $home = realpath(getenv('HOME') ?: getenv('USERPROFILE') ?: '/');
-    // abort when we reach the home dir or top of the filesystem
-    while (dirname($dir) !== $dir && $dir !== $home) {
-      if (file_exists($dir . '/' . Factory::getComposerFile())) {
-        if ($io->askConfirmation('<info>No composer.json in current directory, do you want to use the one at ' . $dir . '?</info> [<comment>Y,n</comment>]? ', TRUE)) {
-          $oldWorkingDir = getcwd();
-          chdir($dir);
-        }
-        break;
-      }
-      $dir = dirname($dir);
-    }
+    parent::configure();
   }
 
   /**
